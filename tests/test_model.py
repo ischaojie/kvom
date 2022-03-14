e41876656a11daefe4e0c8b93693e8e89b9ee77a
ischaojie <zhuzhezhe95@gmail.com>
2022-03-14 16:49:52 +0800

    feat: primary key with test

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,6 +2,7 @@
 import pytest
 
 from kvom.exceptions import NoSourceError
+from kvom.field import Field
 from kvom.model import BaseModel
 from kvom.source import Source
 
@@ -9,7 +10,7 @@ from kvom.source import Source
 class Movie(BaseModel):
     class Meta:
         source = Source("redis://localhost:6379/0")
-        model_key_prefix = "movie"
+        prefix = "movie"
         encoding = "utf-8"
 
     title: str
@@ -25,42 +26,41 @@ def test_model_meta_default():
         class Meta:
             source = Source("redis://localhost:6379/0")
 
-    assert User.Meta.global_key_prefix == ""
-    assert User.Meta.model_key_prefix == "tests.test_model:user"
+    assert User.Meta.prefix == "tests.test_model:user"
     assert User.Meta.encoding == "utf-8"
     assert not User.Meta.embedded
-    assert User.Meta.db_key
+    assert User.Meta.primary_key == "pk"
 
     user = User(name="John Doe", age=42)
-    assert user.key.startswith("tests.test_model:user:")
+    assert user.key == "tests.test_model:user:pk"
 
 
-def test_model_meta_no_defined_default():
+def test_model_meta_custom():
     class User(BaseModel):
         class Meta:
             source = Source("redis://localhost:6379/0")
-            db_key = "test"
             embedded = True
             encoding = "utf-16"
+            primary_key = "name"
+            prefix = "custom_prefix"
 
-        name: str
+        name: str = Field(default="chaojie")
         age: int
 
-    assert User.Meta.global_key_prefix == ""
-    assert User.Meta.model_key_prefix == "tests.test_model:user"
+    assert User.Meta.prefix == "custom_prefix"
     assert User.Meta.encoding == "utf-16"
     assert User.Meta.embedded
-    assert User.Meta.db_key == "test"
+    assert User.Meta.primary_key == "name"
 
     user = User(name="test", age=10)
-    assert user.key == "tests.test_model:user:test"
+    assert user.key == "user:name"
 
 
 def test_model_meta_inherited():
     class User(BaseModel):
         class Meta:
             source = Source("redis://localhost:6379/0")
-            model_key_prefix = "test"
+            prefix = "test"
             encoding = "utf-8"
 
         name: str
@@ -69,27 +69,13 @@ def test_model_meta_inherited():
     class Admin(User):
         pass
 
-    assert Admin.Meta.global_key_prefix == ""
-    assert Admin.Meta.model_key_prefix == "tests.test_model:admin"
+    assert Admin.Meta.prefix == "test"
     assert Admin.Meta.encoding == "utf-8"
 
 
 def test_model_key_model_key_prefix():
     movie = Movie(title="The Matrix", year=1999, director="Lana Wachowski")
-    assert movie.key.startswith("movie:")
-
-
-def test_model_key_global_key_prefix():
-    class Te(BaseModel):
-        class Meta:
-            source = Source("redis://localhost:6379/0")
-            global_key_prefix = "test"
-            model_key_prefix = "te"
-
-        title: str
-
-    t = Te(title="The Matrix")
-    assert t.key.startswith("test:te:")
+    assert movie.key == "movie:pk"
 
 
 def test_model_meta_source():
